4e7e7786830e034a8e32b27b37e49a6107e1c7d8
awesomlypossom <awesomlypossom@gmail.com>
2020-11-09 09:56:38 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@
 //When system is on motor will rotate based on potentiometer location 
 //System turns off when button is pushed
 
+// Create states for the debounce machine.
+typedef enum stateType_enum{wait_press, dbpress, waitrelse, dbrelse} stateType;
+volatile stateType state = wait_press;
 
 int main()
 {
@@ -22,13 +25,35 @@ int main()
 
     while(1)
     {
+      /////////////////////////////////////////////////////debounce state machine
+ switch(state){
+        case wait_press:
+        break;
+        case dbpress:
+        _delay_us(100);
+        state = waitrelse;
+        break;
+        case waitrelse:
+        break;
+        case dbrelse:
+        
+        _delay_us(100);
+        state = wait_press;
+    ///////////////////////////////////////////////////////////////////CHANGE OF THE DUTY CYCLE
+        break;
+        }
 
-    
+         
     }
 
     return 0;
 }
 // Establish ISR using external interput on PORTD
-  ISR(){
-  
+  ISR(INT0_vect){
+    if(state == wait_press){//code to change state based on current state
+     state = dbpress;
+    }
+    if(state == waitrelse){
+    state = dbrelse;
+    }
 } 
