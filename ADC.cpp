1486312de4819d5d2e063ffd645ee99f99329eda
RafeedArian <74168589+RafeedArian@users.noreply.github.com>
2020-11-08 21:57:51 -0700

    Update ADC.cpp

--- ADC.cpp
+++ ADC.cpp
@@ -12,6 +12,27 @@
   
 void initADC()
 {
+    // Set reference voltage to be AVCC
+    ADMUX |= (1<<REFS0);
+    ADMUX &= ~(1<<REFS1);
 
+    // Set to right justified
+    ADMUX &= ~(1<<ADLAR);
+
+    // Set Auto-trigger Source Selection
+    // Set to Free-running
+    ADCSRB &= ~((1<<ADTS2) | (1<<ADTS1) | (1<<ADTS0));
+
+    // Enable auto-triggering and turn on ADC
+    ADCSRA |= (1<<ADEN) | (1<<ADATE);
+
+    // Set prescalar to 128
+    ADCSRA |= (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+
+    // Disable ADC0 pin digital input
+    DIDR0 |= (1<<ADC7D);
+
+    // Start first conversion
+    ADCSRA |= (1<<ADSC);
 }
 
